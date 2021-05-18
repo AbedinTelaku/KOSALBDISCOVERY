3e42ea924a00774914006b3020c2c76af0beac7f
eltonboshnjaku <eb44025@ubt-uni.net>
2021-05-18 23:50:50 +0200

    businessModel

--- src/main/java/com/example/demo/Register/Models/Business.java
+++ src/main/java/com/example/demo/Register/Models/Business.java
@@ -1,8 +1,129 @@
 package com.example.demo.Register.Models;
 
 import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 public class Business {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
+    private int Business_ID;
+    @Column
+    private String name;
+    @Column
+    private String email;
+    @Column
+    private String password;
+    @Column
+    private String username;
+    @Column
+    private String tel_Number;
+    @Column
+    private String owner_ID;
+    @Column
+    private String fiscal_Number;
+    @Column
+    private String business_Number;
+    @Column
+    private String business_Activity;
+
+
+    public Business(int business_ID, String name, String email, String password, String username, String tel_Number, String owner_ID, String fiscal_Number, String business_Number, String business_Activity) {
+        Business_ID = business_ID;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        this.tel_Number = tel_Number;
+        this.owner_ID = owner_ID;
+        this.fiscal_Number = fiscal_Number;
+        this.business_Number = business_Number;
+        this.business_Activity = business_Activity;
+    }
+
+    public Business() {
+
+    }
+
+    public int getBusiness_ID() {
+        return Business_ID;
+    }
+
+    public void setBusiness_ID(int business_ID) {
+        Business_ID = business_ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getTel_Number() {
+        return tel_Number;
+    }
+
+    public void setTel_Number(String tel_Number) {
+        this.tel_Number = tel_Number;
+    }
+
+    public String getOwner_ID() {
+        return owner_ID;
+    }
+
+    public void setOwner_ID(String owner_ID) {
+        this.owner_ID = owner_ID;
+    }
+
+    public String getFiscal_Number() {
+        return fiscal_Number;
+    }
+
+    public void setFiscal_Number(String fiscal_Number) {
+        this.fiscal_Number = fiscal_Number;
+    }
+
+    public String getBusiness_Number() {
+        return business_Number;
+    }
+
+    public void setBusiness_Number(String business_Number) {
+        this.business_Number = business_Number;
+    }
+
+    public String getBusiness_Activity() {
+        return business_Activity;
+    }
+
+    public void setBusiness_Activity(String business_Activity) {
+        this.business_Activity = business_Activity;
+    }
 }
