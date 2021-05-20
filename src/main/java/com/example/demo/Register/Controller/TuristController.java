f89096a30a505d8d0f910bcb65a86e6e224289a8
veprimmorina <65460301+veprimmorina@users.noreply.github.com>
2021-05-20 13:48:00 +0200

    controller

--- src/main/java/com/example/demo/Register/Controller/TuristController.java
+++ src/main/java/com/example/demo/Register/Controller/TuristController.java
@@ -2,12 +2,17 @@ package com.example.Register.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.Register.Services.ITuristService;
 
 
 
 @RestController
 @RequestMapping("/api/turist")
 public class TuristController{
-    
+
+    @Autowired
+    private ITuristService turistService;
+
 
 }
