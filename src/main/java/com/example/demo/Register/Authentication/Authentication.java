e3413f514db0513d605566c1a9f50e673a3df388
eltonboshnjaku <eb44025@ubt-uni.net>
2021-05-21 23:43:46 +0200

    Update Authentication.java

--- src/main/java/com/example/demo/Register/Authentication/Authentication.java
+++ src/main/java/com/example/demo/Register/Authentication/Authentication.java
@@ -1,10 +1,12 @@
 package com.example.demo.Register.Authentication;
 
-import com.example.demo.Register.Models.Business;
-import com.example.demo.Register.Repository.BusinessRepository;
 import com.example.demo.Register.Controller.TouristController;
+import com.example.demo.Register.Models.Business;
 import com.example.demo.Register.Models.Tourist;
+import com.example.demo.Register.Repository.BusinessRepository;
 import com.example.demo.Register.Repository.TouristRepository;
+
+import java.util.List;
 
 public class Authentication implements AuthenticationInterface {
 
@@ -14,90 +16,89 @@ public class Authentication implements AuthenticationInterface {
     private TouristRepository touristRepository;
 
 
-
-    public Authentication(BusinessRepository businessRepository){
-        this.businessRepository=businessRepository;
+    public Authentication(BusinessRepository businessRepository) {
+        this.businessRepository = businessRepository;
     }
 
     @Override
     public boolean checkTouristPassword(String password) {
 
-         boolean usernameExist = checkTouristUsername(username);
-        Tourist tourist=null;
-        if(usernameExist == true){
+        boolean usernameExist = checkTouristUsername(username);
+        Tourist tourist = null;
+        if (usernameExist == true) {
             tourist = this.touristRepository.findTouristByUsername(username);
         }
-        if(tourist.getPassword().equals(password)){
+        if (tourist.getPassword().equals(password)) {
             return true;
-        }else{
+        } else {
             return false;
+        }
     }
+        @Override
+        public boolean checkTouristUsername (String username){
+            List<Tourist> tourists = this.touristRepository.findAll();
+            int result = 0;
 
-    @Override
-    public boolean checkTouristUsername(String username) {
-        List<Tourist> tourists = this.touristRepository.findAll();
-        int result=0;
-
-        for (int i=0; i<tourists.size();i++){
-            Tourist tourist = tourists.get(i);
-            if(tourist.getEmail().equals(username) || tourist.getUsername().equals(username)){
-                result=1;
-                break;
-            }else{
-                result=-1;
+            for (int i = 0; i < tourists.size(); i++) {
+                Tourist tourist = tourists.get(i);
+                if (tourist.getEmail().equals(username) || tourist.getUsername().equals(username)) {
+                    result = 1;
+                    break;
+                } else {
+                    result = -1;
+                }
+            }
+            if (result == 1) {
+                return true;
+            } else {
+                return false;
             }
         }
-        if(result==1){
-            return true;
-        }else{
+
+        @Override
+        public boolean checkBusinessPassword (String username, String password){
+            boolean usernameExist = checkBusinessUsername(username);
+            Business business = null;
+            if (usernameExist == true) {
+                business = this.businessRepository.findBusinessByUsername(username);
+            }
+            if (business.getPassword().equals(password)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        public boolean checkBusinessUsername (String username){
+
+            Business business = this.businessRepository.findBusinessByUsername(username);
+
+            if (business != null) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        @Override
+        public boolean checkIfBusinessExist (String username, String password){
+            boolean usernameExist = checkBusinessUsername(username);
+            boolean passwordExist = checkBusinessPassword(username, password);
+
+            if (usernameExist == true && passwordExist == true) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        @Override
+        public boolean checkAdminPassword (String password){
+            return false;
+        }
+
+        @Override
+        public boolean checkAdminUsername (String username){
             return false;
         }
     }
-
-    @Override
-    public boolean checkBusinessPassword(String username, String password) {
-        boolean usernameExist = checkBusinessUsername(username);
-        Business business=null;
-        if(usernameExist == true){
-            business = this.businessRepository.findBusinessByUsername(username);
-        }
-        if(business.getPassword().equals(password)){
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-    @Override
-    public boolean checkBusinessUsername(String username) {
-
-        Business business = this.businessRepository.findBusinessByUsername(username);
-
-        if(business != null){
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-    @Override
-    public boolean checkIfBusinessExist(String username, String password){
-        boolean usernameExist = checkBusinessUsername(username);
-        boolean passwordExist = checkBusinessPassword(username,password);
-
-        if(usernameExist == true && passwordExist == true){
-            return true;
-        }else{
-            return false;
-        }
-    }
-    @Override
-    public boolean checkAdminPassword(String password) {
-        return false;
-    }
-
-    @Override
-    public boolean checkAdminUsername(String username) {
-        return false;
-    }
-}
