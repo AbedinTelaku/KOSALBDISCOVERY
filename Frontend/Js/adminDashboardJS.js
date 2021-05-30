c495fa43614dd2299a35432583da91f48707386a
eltonboshnjaku <eb44025@ubt-uni.net>
2021-05-31 01:09:57 +0200

    darkMode

--- Frontend/Js/adminDashboardJS.js
+++ Frontend/Js/adminDashboardJS.js
@@ -1,54 +1,35 @@
-var editReservations = document.getElementById("editReservations");
-var newReservation = document.getElementById("newReservation");
-var reservationsButton = document.getElementById("reservationsButton");
 
-$("#").click(function (event){
-    if(editReservations.classList.value=="list-group-item hide"){
 
-        reservationsButton.classList.add("active");
-        editReservations.classList.remove("hide");
-        newReservation.classList.remove("hide");
 
-    }else{
-        reservationsButton.classList.remove("active");
-        editReservations.classList.add("hide");
-        newReservation.classList.add("hide");
+var mainContent = document.getElementById("bodyContent");
+var headerContent = document.getElementById("headerContent");
+
+var darkModeCheck=document.getElementById("darkModeSwitchCheck");
+var lightModeCheck=document.getElementById("lightModeSwitchCheck");
+
+  
+  $("#darkModeSwitchCheck").on('change', function() {
+      if ($(this).is(':checked')) {
+          mainContent.classList.add("dark-Mode-One");
+          headerContent.classList.add("dark-Mode-Zero");
+
+         
+      }
+      else {
+         mainContent.classList.remove("dark-Mode-One");
+         headerContent.classList.remove("dark-Mode-Zero");
+      }
+  });
+
+  $("#lightModeSwitchCheck").on('change', function() {
+    if ($(this).is(':checked')) {
+        mainContent.classList.remove("dark-Mode-One");
+        headerContent.classList.remove("dark-Mode-Zero");
+
+       
     }
-})
-
-var tourists = document.getElementById("tourists");
-var businesses = document.getElementById("businesses");
-var usersButton = document.getElementById("usersButton");
-
-$("#").click(function (event){
-    if(tourists.classList.value=="list-group-item hide"){
-
-        usersButton.classList.add("active");
-        tourists.classList.remove("hide");
-        businesses.classList.remove("hide");
-    }else{
-        usersButton.classList.remove("active");
-        tourists.classList.add("hide");
-        businesses.classList.add("hide");
+    else {
+       mainContent.classList.add("dark-Mode-One");
+       headerContent.classList.add("dark-Mode-Zero");
     }
-})
-
-
-var editEvents = document.getElementById("editEvents");
-var newEvent = document.getElementById("newEvent");
-var eventsButton = document.getElementById("eventsButton");
-
-
-$("#").click(function (event) {
-    if(editEvents.classList.value=="list-group-item hide"){
-
-        eventsButton.classList.add("active");
-        editEvents.classList.remove("hide");
-        newEvent.classList.remove("hide");
-    }else{
-        eventsButton.classList.remove("active");
-        editEvents.classList.add("hide");
-        newEvent.classList.add("hide");
-    }
-})
-
+});
