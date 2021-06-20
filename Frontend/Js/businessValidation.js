7ac76a67ea02daff4c27d6e55f4f421acf6e14e7
AbedinTelaku <at45490@ubt-uni.net>
2021-06-21 01:00:45 +0200

    ValidationBusinessRegister

--- Frontend/Js/businessValidation.js
+++ Frontend/Js/businessValidation.js
@@ -1,15 +1,54 @@
-function myFunction() {
-    var x;
-    var text;
-
-
-    x = document.getElementById("full_name").value;
-
-
-    if (full_name=="") {
-        text = "Input not valid";
-    } else {
-        text = "Input OK";
+function loginValidate(){
+    if(document.myForm.fullname.value == "" || document.myForm.fullname.value ==null){
+        alert("Name is required");
+        return false;
     }
-    document.getElementById("demo").innerHTML = text;
+    if(document.myForm.email.value == ""){
+        alert("Email is required");
+        document.myForm.email.focus();
+        return false;
+    }
+    if(document.myForm.password.value == "" ){
+        alert("Password is required");
+        document.myForm.password.focus();
+        return false;
+    }
+    if(document.myForm.password.value.length < 6){
+        alert("Passowrd should contain at least 6 characters");
+        document.myForm.password.focus();
+        return false;
+    }
+    if(document.myForm.username.value == ""){
+        alert("Username is required");
+        document.myForm.username.focus();
+        return false;
+    }
+    if(document.myForm.username.value.length < 6){
+        alert("Username should contain at least 6 characters");
+        document.myForm.username.focus();
+        return false;
+    }
+
+
+    if(document.myForm.phonenumber.value == ""){
+        alert("Phone number is required");
+        document.myForm.phonenumber.focus();
+        return false;
+    }
+    if(document.myForm.ownerid.value == "" ){
+        alert("Owner id is required");
+        document.myForm.ownerid.focus();
+        return false;
+    }
+    if(document.myForm.fiscalnumber.value == ""){
+        alert("Fiscal number is required");
+        document.myForm.fiscalnumber.focus();
+        return false;
+    }
+    if(document.myForm.businessnumber.value == ""){
+        alert("Business number is required");
+        document.myForm.businessnumber.focus();
+        return false;
+    }
+
 }
