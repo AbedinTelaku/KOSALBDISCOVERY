7845049dc708b6fd6fb2625ae1c205d63c0164cf
veprimmorina <65460301+veprimmorina@users.noreply.github.com>
2021-06-29 20:35:47 +0200

    rabbit/unsynchronized

--- EventsService/src/main/java/com/example/demo/Infrastructure/OutputAdapter/NotificationOutputAdapter.java
+++ EventsService/src/main/java/com/example/demo/Infrastructure/OutputAdapter/NotificationOutputAdapter.java
@@ -2,6 +2,10 @@ package com.example.demo.Infrastructure.OutputAdapter;
 
 import com.example.demo.Core.Helper.UserHelper;
 import com.example.demo.Core.OutputPort.NotificationOutputPort;
+import com.example.demo.Infrastructure.Helper.UserRabbitmqHelper;
+import com.example.demo.RabbitConfiguration.MessagingConfiguration;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -13,7 +17,21 @@ import javax.validation.constraints.Email;
 
 @Service
 public class NotificationOutputAdapter implements NotificationOutputPort {
+
+    @Autowired
+    private RabbitTemplate template;
+
     @Override
+    public void sendEmail(String email) {
+
+        UserRabbitmqHelper userRabbitmqHelper=new UserRabbitmqHelper(email);
+        template.convertAndSend(MessagingConfiguration.EXCHANGE, MessagingConfiguration.KEY, userRabbitmqHelper);
+
+
+    }
+
+
+    /*@Override
     public void sendEmail(String email) {
         RestTemplate restTemplate = new RestTemplate();
 
@@ -26,4 +44,6 @@ public class NotificationOutputAdapter implements NotificationOutputPort {
 
 
     }
+
+     */
 }
