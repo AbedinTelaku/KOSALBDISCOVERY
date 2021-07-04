88921e0d35559cd28642b07dad9a6c021e9e37b6
AbedinTelaku <at45490@ubt-uni.net>
2021-07-05 01:39:19 +0200

    update

--- Frontend/Js/businessRegisterJs.js
+++ Frontend/Js/businessRegisterJs.js
@@ -1,107 +1,75 @@
-// function validateForm() {
-//     var fullname = document.myForm.fullname.value;
-//     var email = document.myForm.email.value;
-//     var businessPassword = document.myForm("password").value;
-//     var businessUsername = document.myForm("user_name").value;
-//     var businessPhoneNumber = document.myForm("phonenumber").value;
-//     var businessOwnerId = document.myForm("ownerid").value;
-//     var businessFiscalNumber = document.myForm("fiscalnumber").value;
-//     var businessNum = document.myForm("businessnumber").value;
-//     var businessActi = document.myForm("businessactivity").value;
-//     var statuss = "activ"
-//     var rolee = "business";
-//
-//
-//     function printError(elemId, hintMsg) {
-//         document.getElementById(elemId).innerHTML = hintMsg;
-//     }
-//
-//     var nameErr = emailErr  = true;
-//
-//     // Validate name
-//     if(fullname == "") {
-//         printError("nameErr", "Please enter your name");
-//     } else {
-//         var regex = /^[a-zA-Z\s]+$/;
-//         if(regex.test(fullname) === false) {
-//             printError("nameErr", "Please enter a valid name");
-//         } else {
-//             printError("nameErr", "");
-//             nameErr = false;
-//         }
-//     }
-//
-//     // Validate email address
-//     if(email == "") {
-//         printError("emailErr", "Please enter your email address");
-//     } else {
-//         // Regular expression for basic email validation
-//         var regex = /^\S+@\S+\.\S+$/;
-//         if(regex.test(email) === false) {
-//             printError("emailErr", "Please enter a valid email address");
-//         } else{
-//             printError("emailErr", "");
-//             emailErr = false;
-//         }
-//     }
-//
-//
-//     else{
-//         business = {
-//             name: businessName,
-//             email: businessEmail,
-//             password: businessPassword,
-//             username: businessUsername,
-//             tel_Number: businessPhoneNumber,
-//             owner_ID: businessOwnerId,
-//             fiscal_Number: businessFiscalNumber,
-//             business_Number: businessNum,
-//             business_Activity: businessActi,
-//             status: statuss,
-//             role: rolee
-//
-//         }
-//         console.log(business)
-//
-//         registerBusiness(business);
-//     }
-// });
-//
-//
-// function registerBusiness(business) {
-//
-//
-//     $.ajax({
-//         url: "http://localhost:8080/api/business/create/business",
-//         type: 'post',
-//         contentType: "application/json; charset=utf-8",
-//         // dataType: "json",
-//         data: JSON.stringify(business),
-//         success: function (res) {
-//             // localStorage.setItem('kuizi', JSON.stringify(res))
-//
-//             console.log(business)
-//
-//
-//         },
-//         error: function (request, status, error) {
-//             console.log(error);
-//             console.log(status);
-//         }
-//     })
-// }
-//
-//
-// var business = {
-//     name: "",
-//     email: "",
-//     password: "",
-//     username: "",
-//     tel_Number: "",
-//     owner_ID: "",
-//     fiscal_Number: "",
-//     business_Number: "",
-//     business_Activity: "",
-//     status: "",
-//     role: ""
-// }
+$("#registerButton").click(function (event) {
+
+    var businessName = document.getElementById("full_name").value;
+    var businessEmail = document.getElementById("email").value;
+    var businessPassword = document.getElementById("password").value;
+    var businessUsername = document.getElementById("user_name").value;
+    var businessPhoneNumber = document.getElementById("phonenumber").value;
+    var businessOwnerId = document.getElementById("ownerid").value;
+    var businessFiscalNumber = document.getElementById("fiscalnumber").value;
+    var businessNum = document.getElementById("businessnumber").value;
+    var businessActi = document.getElementById("businessactivity").value;
+    var statuss = "activ"
+    var rolee = "business";
+
+
+
+
+    business = {
+        name: businessName,
+        email: businessEmail,
+        password: businessPassword,
+        username: businessUsername,
+        tel_Number: businessPhoneNumber,
+        owner_ID: businessOwnerId,
+        fiscal_Number: businessFiscalNumber,
+        business_Number: businessNum,
+        business_Activity: businessActi,
+        status: statuss,
+        role: rolee
+    }
+
+    console.log(business)
+
+    registerBusiness(business);
+
+});
+
+
+function registerBusiness(business) {
+
+
+    $.ajax({
+        url: "http://localhost:9000/api/business/create/business",
+        type: 'post',
+        contentType: "application/json; charset=utf-8",
+        // dataType: "json",
+        data: JSON.stringify(business),
+        success: function (res) {
+            // localStorage.setItem('kuizi', JSON.stringify(res))
+
+            console.log(business)
+
+
+        },
+        error: function (request, status, error) {
+            console.log(error);
+            console.log(status);
+        }
+    })
+}
+
+
+var business = {
+    name: "",
+    email: "",
+    password: "",
+    username: "",
+    tel_Number: "",
+    owner_ID: "",
+    fiscal_Number: "",
+    business_Number: "",
+    business_Activity: "",
+    status: "",
+    role: ""
+}
